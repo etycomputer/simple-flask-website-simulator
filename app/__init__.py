f3f4a04bc69273321968aba8c915202f8d71d27c
etycomputer-github <etycomputer@gmail.com>
2021-04-11 15:04:05 +1200

    📦️ Updating the App package to use template-layouts for the home page 📦️ Added home page to the App package

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(
         __name__,
+        template_folder='templates',
         instance_relative_config=True
     )
 
@@ -12,3 +13,8 @@ def create_app():
     def hello():
         return 'Hello World!', 200
 
+    @app.route('/')
+    def home():
+        return render_template('base_layout.html', message="Hello World!")
+
+    return app
