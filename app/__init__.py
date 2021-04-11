3b0e68164daa56cae9677ad3acc9590a45220218
etycomputer-github <etycomputer@gmail.com>
2021-04-11 16:53:51 +1200

    ✨ Added template HTML to the root of that APP 💄 Added ICONs 💄 Added styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from app.routes import bp as static_content_bp
 
 
 def create_app():
@@ -8,6 +9,8 @@ def create_app():
         template_folder='templates',
         instance_relative_config=True
     )
+    app.debug = True
+    app.register_blueprint(static_content_bp, url_prefix='/')
 
     @app.route('/hello', methods=['GET'])
     def hello():
---
+++ app/routes/static_content.py
@@ -0,0 +1,48 @@
+import os
+
+from flask import send_from_directory
+
+from app.routes import bp
+
+
+@bp.route('/icon/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(bp.root_path, '..', 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@bp.route('/icon/favicon-16x16.png')
+def favicon_16_png():
+    return send_from_directory(os.path.join(bp.root_path, '..', 'static'),
+                               'favicon-16x16.png', mimetype='image/png')
+
+
+@bp.route('/icon/favicon-32x32.png')
+def favicon_32_png():
+    return send_from_directory(os.path.join(bp.root_path, '..', 'static'),
+                               'favicon-32x32.png', mimetype='image/png')
+
+
+@bp.route('/icon/apple-touch-icon.png')
+def apple_png():
+    return send_from_directory(os.path.join(bp.root_path, '..', 'static'),
+                               'apple-touch-icon.png', mimetype='image/png')
+
+
+@bp.route('/icon/android-chrome-192x192.png')
+def android_chrome_192_png():
+    return send_from_directory(os.path.join(bp.root_path, '..', 'static'),
+                               'android-chrome-192x192.png', mimetype='image/png')
+
+
+@bp.route('/icon/android-chrome-512x512.png')
+def android_chrome_512_png():
+    return send_from_directory(os.path.join(bp.root_path, '..', 'static'),
+                               'android-chrome-512x512.png', mimetype='image/png')
+
+
+@bp.route('/css/style.css')
+def style_css():
+    print(os.path.join(bp.root_path, '..', 'static'))
+    return send_from_directory(os.path.join(bp.root_path, '..', 'static'),
+                               'style.css', mimetype='text/css')
